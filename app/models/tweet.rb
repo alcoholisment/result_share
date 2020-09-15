5d30ada4ebd21cfa5ff594735f13ef98b94f4e05
alcoholisment <alcoholism1025@gmail.com>
2020-09-15 17:40:02 +0900

    調整

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -2,7 +2,7 @@ class Tweet < ApplicationRecord
   belongs_to :user
   has_many :comments
   has_one_attached :image
-  validates :text
+  validates :text, length: { maximum: 100 }
 
   def self.search(search)
     if search
